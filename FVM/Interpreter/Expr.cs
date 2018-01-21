58b308a23d92834f860f3e9fa45ced1797f9f3cb
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-21 16:45:30 +0900

    ConstantExprクラスを作成した

--- FVM/Interpreter/Expr.cs
+++ FVM/Interpreter/Expr.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace FVM.Interpreter
 {
-    class Expr
+    //ベースとなる式
+    interface Expr<T>
     {
+        T Result();
     }
+
+    //定数を返す式
+    class ConstantExpr<T> : Expr<T>
+    {
+        T value;
+        public ConstantExpr(T value)
+        {
+            this.value = value;
+        }
+
+        public T Result()
+        {
+            return value;
+        }
+    }
+
 }
