835efc3a3a65ddd30cf4015232c26e57df7e4a38
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-21 16:49:36 +0900

    output命令を実装

---
+++ FVM/Interpreter/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FVM.Interpreter
+{
+    static class Order
+    {
+        static void output<T>(Expr<T> expr)
+        {
+            Console.WriteLine(expr.Result());
+        }
+    }
+}
